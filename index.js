c9cc58d614316582f31c1a9687f21331f284783e
Nasir-Saeed <92154367+Nasir-Saeed@users.noreply.github.com>
2021-10-29 22:25:53 +0500

    Add files via upload
    Init Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/profile', (req, res) => {
+  res.send('This is Our Profile')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
